741d6d2e0e40dc9f6f74e3aded1b1b6f6900625a
Spidey00007 <asamit1547k@gmail.com>
2026-02-10 12:32:15 +0530

    eror

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -27,6 +27,8 @@ const Line = styled(motion.span)`
 `;
 
 const SocialIcons = (props) => {
+  const iconColor = props.theme === "dark" ? DarkTheme.text : DarkTheme.body;
+
   return (
     <Icons>
       <motion.div
@@ -35,81 +37,65 @@ const SocialIcons = (props) => {
         transition={{ type: "spring", duration: 1, delay: 1 }}
       >
         <a
-          style={{ color: "inherit" }}
+          href="https://github.com/vikash815"
           target="_blank"
-          rel="noreferrer"
-          href={"https://github.com/vikash815"}
+          rel="noopener noreferrer"
+          style={{ color: "inherit" }}
         >
-          <Github
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
+          <Github width={25} height={25} fill={iconColor} />
         </a>
       </motion.div>
+
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1.2 }}
       >
         <a
-          style={{ color: "inherit" }}
+          href="mailto:gvikashkumar37@gmail.com"
           target="_blank"
-          rel="noreferrer"
-          href={"mailto:gvikashkumar37@gmail.com"}
+          rel="noopener noreferrer"
+          style={{ color: "inherit" }}
         >
-          <Gmail
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
+          <Gmail width={25} height={25} fill={iconColor} />
         </a>
       </motion.div>
+
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1.4 }}
       >
         <a
-          style={{ color: "inherit" }}
+          href="https://linkedin.com/in/vikash815"
           target="_blank"
-          rel="noreferrer"
-          href={"https://linkedin.com/in/vikash815"}
+          rel="noopener noreferrer"
+          style={{ color: "inherit" }}
         >
-          <LinkedIn
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
+          <LinkedIn width={25} height={25} fill={iconColor} />
         </a>
       </motion.div>
+
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: [0, 1, 1.5, 1] }}
         transition={{ type: "spring", duration: 1, delay: 1.6 }}
       >
-        <a style={{ color: "inherit" }} target="_blank" rel="noreferrer" href={""}>
-          <LeetCode
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
+        <a
+          href="https://leetcode.com/u/vikash815/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "inherit" }}
+        >
+          <LeetCode width={25} height={25} fill={iconColor} />
         </a>
       </motion.div>
 
       <Line
         color={props.theme}
-        initial={{
-          height: 0,
-        }}
-        animate={{
-          height: "8rem",
-        }}
-        transition={{
-          type: "spring",
-          duration: 1,
-          delay: 0.8,
-        }}
+        initial={{ height: 0 }}
+        animate={{ height: "8rem" }}
+        transition={{ type: "spring", duration: 1, delay: 0.8 }}
       />
     </Icons>
   );
